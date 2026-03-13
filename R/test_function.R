9c401c5575947501106ce04f86317df5d46dd219
Scott Kreider <scott.kreider@thermofisher.com>
2026-03-13 11:11:19 -0400

    Added first function

---
+++ R/test_function.R
@@ -0,0 +1,12 @@
+#' Test function for educational purposes
+#'
+#' @param num1 First number
+#' @param num2 Second number
+#'
+#' @returns A sum of the 2 numbers
+#' @export
+test_function <- function(num1, num2) {
+  result <- num1 + num2
+
+  return(result)
+}
