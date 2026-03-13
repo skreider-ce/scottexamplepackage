c22b91487c8f50bdbbbad16abe85e857ebc37497
Scott Kreider <scott.kreider@thermofisher.com>
2026-03-13 11:20:48 -0400

    Updated test_function
    - Changed second number to square

--- R/test_function.R
+++ R/test_function.R
@@ -3,10 +3,10 @@
 #' @param num1 First number
 #' @param num2 Second number
 #'
-#' @returns A sum of the 2 numbers
+#' @returns A sum of num1 + num2 squared
 #' @export
 test_function <- function(num1, num2) {
-  result <- num1 + num2
+  result <- num1 + num2**2
 
   return(result)
 }
