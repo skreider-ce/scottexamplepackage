324e7f74c20b10342f7800b50e5638d4ca2b5f69
Scott Kreider <scott.kreider@thermofisher.com>
2026-03-13 13:30:37 -0400

    Add a glue import and print result to the console

--- R/test_function.R
+++ R/test_function.R
@@ -3,10 +3,14 @@
 #' @param num1 First number
 #' @param num2 Second number
 #'
+#' @importFrom glue glue
+#'
 #' @returns A sum of num1 + num2 squared
 #' @export
 test_function <- function(num1, num2) {
   result <- num1 + num2**2
+
+  print(glue::glue("The result is: {result}"))
 
   return(result)
 }
